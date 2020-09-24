be0a6cd5bf2cb09884204c8fb330ce71846093c2
Schoperation <cschop1@gmail.com>
2020-09-24 13:22:44 -0400

    Can parse json files

--- speech/SpeechTree.gd
+++ speech/SpeechTree.gd
@@ -7,6 +7,7 @@ The root node is the start of the conversation. It can be directly accessed at a
 """
 
 class Leaf:
+	var id: int
 	var name: String
 	var dialogue: String # Array
 	var options: Leaf # Array
@@ -20,14 +21,51 @@ class Leaf:
 	# Variables below this comment are set in-game
 	var read: bool
 	
-	func _init(name, dialogue, options, goesBack, goesBackMsg):
+	func _init(name, dialogue, options, goesBack, goesBackMsg, hasCondition, condition):
 		self.name = name
 		self.dialogue = dialogue
 		self.options = options
 		self.goesBack = goesBack
 		self.goesBackMsg = goesBackMsg
+		self.hasCondition = hasCondition
+		self.condition = condition
+		
+
+# Actual tree class that stores leaves
+class SpeechTree:
+	var name: String
+	var leaves: Leaf # Array
+	
+	var rootLeaf: Leaf
+	
+	func _init(name, leaves, rootLeaf):
+		self.name = name
+		self.leaves = leaves
+		self.rootLeaf = rootLeaf
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var array = [0, 1, 2]
+	createSpeechTree("res://speech/sample.json")
+	
+func openJSON(filePath) -> Dictionary:
+	var file = File.new()
+	file.open(filePath, file.READ)
+	
+	# Grab contents and put them in a dictionary
+	var content = file.get_as_text()
+	var dict = {}
+	dict = parse_json(content)
+	file.close()
+	return dict
+	
+func createSpeechTree(filePath):
+	var dict = openJSON(filePath)
+	#print(dict)
+	
+	print(dict[1]["choice"])
+	
+	
+	
+	
+
 	
